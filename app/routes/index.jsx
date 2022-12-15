c4767dd64981a4e05ea6877b62298b36e795be87
Jaster <jaster@oen.tw>
2022-12-15 11:36:02 +0800

    init

---
+++ app/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div className="flex w-full h-screen bg-blue-300/20">
+      <h1 className="text-2xl m-auto">Hello World</h1>
+    </div>
+  )
+}
